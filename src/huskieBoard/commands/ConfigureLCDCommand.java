a8fa82bd0466e0b36228f2e1ee7d26a69c2b11e7
Brandon John <wiresboy@yahoo.com>
2017-06-17 02:00:20 -0500

    Added ConfigureLCDCommand
    This command still is not documented anywhere, had to figure it out
    based on the spin code.

---
+++ src/huskieBoard/commands/ConfigureLCDCommand.java
@@ -0,0 +1,65 @@
+/**
+ * Configure the size of the LCD screen attached to the Huskie Board
+ */
+package huskieBoard.commands;
+
+/**
+ * @author Brandon John
+ * Configure the size of the LCD screen attached to the Huskie Board
+ */
+public class ConfigureLCDCommand extends Command {
+
+	private static final byte commandByte = 0x09;
+	private byte[] configureLCDBytes = null;
+	
+	/**
+	 * @param lines How large is the LCD display (options: 2,4)
+	 * @param priority
+	 * @throws Exception 
+	 */
+	public ConfigureLCDCommand(int lines, int priority) throws Exception{
+		super(commandByte, priority);
+		if (lines == 2)
+			configureLCDBytes = new byte[] {commandByte, 0x02, commandByte+0x02};
+		else if (lines == 4)
+			configureLCDBytes = new byte[] {commandByte, 0x04, commandByte+0x04};
+		else
+			throw new Exception("Only 2 and 4 line displays are accepted");
+	}
+
+	/**
+	 * @param lines How large is the LCD display (options: 2,4)
+	 * @throws Exception 
+	 */
+	public ConfigureLCDCommand(int lines) throws Exception {
+		this(lines, kDefaultPriority);
+	}
+
+	/* (non-Javadoc)
+	 * @see huskieBoard.commands.Command#getCommandByteArray()
+	 */
+	@Override
+	public byte[] getCommandByteArray() {
+		return configureLCDBytes;
+	}
+
+	/* (non-Javadoc)
+	 * @see huskieBoard.commands.Command#getExpectedResponseLength()
+	 */
+	@Override
+	public int getExpectedResponseLength() {
+		return 2;
+	}
+
+	/* (non-Javadoc)
+	 * @see huskieBoard.commands.Command#validateResponse(byte[])
+	 */
+	@Override
+	public boolean validateResponse(byte[] response) {
+		if (response.equals(new byte[]{commandByte, commandByte}))
+			return true;
+		else
+			return false;
+	}
+
+}
